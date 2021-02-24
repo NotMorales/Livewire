8913da11e2d2823cf03fc66a8cde3fad4705e46c
NotMorales <luis.antoniomv24@gmail.com>
2021-02-24 10:41:09 -0600

    Instalacion de Jetstream

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/users', function () {
+    return view('users', [
+        'users' => User::paginate()
+    ]);
+})->name('users');
